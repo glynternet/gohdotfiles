3f03e1c7c5560a1c49bde7771f3bed328a857dcf
Glyn Hanmer <glynhanmer@gmail.com>
2016-03-25 15:40:34 +0000

    Added bootstrap script for OSX

---
+++ bootstraposx.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+ROOT_UID="0"
+#Check if run as root
+if [ "$UID" -ne "$ROOT_UID" ] ; then
+	echo "You must be root to run this script!"
+	exit 1
+fi
+
+defaults write com.apple.dock autohide -bool true && defaults write com.apple.dock autohide-delay -float 0 && defaults write com.apple.dock autohide-time-modifier -float 0;
+defaults write com.apple.dock static-only -bool TRUE;
+killall Dock
+
+#Install Homebrew
+cd ~
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install git
+brew install python
+pip install --upgrade pip
+sudo pip install awscli
+sudo pip install awscli --ignore-installed six
+
+cp -r ~/Library/Preferences/com.apple.LaunchServices{,.bak}
+defaults write com.apple.LaunchServices/com.apple.launchservices.secure LSHandlers -array-add \
+'{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.sublimetext.3;}'
