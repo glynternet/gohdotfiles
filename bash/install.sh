443d4f1fb0e9e9ba65b605e6c4a6d8de34f63000
Glyn Hanmer <glynhanmer@gmail.com>
2016-04-01 12:22:46 +0100

    Altered install script to declare GOH_BASH_PROFILE variable for future replacement ease

--- bash/install.sh
+++ bash/install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-printf "\n. $(pwd)/gohbashprofile\n" >> ~/.bash_profile
+#TODO Check for GOH_BASH_PROFILE and handle if already exists. Assume that it was previously installed through script so that the format will be exactly the same, i.e. GOH_BASH_PROFILE='LOCATION'. Just to a replace of LOCATION with $(pwd)
+printf "\nGOH_BASH_PROFILE='$(pwd)/gohbashprofile'\n\${GOH_BASH_PROFILE}" >> ~/.bash_profile
