7e860f1dfcbbb669b40a776f497e728778026747
Orron Fearon <orronfearon@Orrons-iMac-3.local>
2016-04-02 10:54:41 +0100

    Sourced profile instead of executing it to retain environment variables. (I think, could do some further testing to confirm.

--- bash/install.sh
+++ bash/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #TODO Check for GOH_BASH_PROFILE and handle if already exists. Assume that it was previously installed through script so that the format will be exactly the same, i.e. GOH_BASH_PROFILE='LOCATION'. Just to a replace of LOCATION with $(pwd)
-printf "\nGOH_BASH_PROFILE='$(pwd)/gohbashprofile'\n\${GOH_BASH_PROFILE}" >> ~/.bash_profile
+printf "\nGOH_BASH_PROFILE='$(pwd)/gohbashprofile'\n. \${GOH_BASH_PROFILE}" >> ~/.bash_profile
