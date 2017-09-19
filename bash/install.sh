dbd6865594057965d7a177e48d53b26c15f273d4
Glyn Hanmer <glynhanmer@gmail.com>
2017-09-19 18:00:48 +0200

    bash profile name as variable to later be switched for an environment dependant assignment?

--- bash/install.sh
+++ bash/install.sh
@@ -2,6 +2,9 @@
 #run this script from within the gohbashprofile directory
 #TODO Check for GOH_BASH_PROFILE and handle if already exists. Assume that it was previously installed through script so that the format will be exactly the same, i.e. GOH_BASH_PROFILE='LOCATION'. Just to a replace of LOCATION with $(pwd)
 GOH_BASH_PROFILE="$(pwd)/gohbashprofile"
-printf "\nGOH_BASH_PROFILE='$GOH_BASH_PROFILE'\n. \${GOH_BASH_PROFILE}" >> ~/.bash_profile
-echo "$GOH_BASH_PROFILE"
+profile=".bash_profile"
+printf "\nGOH_BASH_PROFILE='$GOH_BASH_PROFILE'\n. \${GOH_BASH_PROFILE}" >> ~/$profile
+echo "Profile located at $GOH_BASH_PROFILE"
+echo "Sourcing profile..."
 . "$GOH_BASH_PROFILE"
+echo "Done"
