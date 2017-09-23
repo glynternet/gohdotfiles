77b2d4c7bb125702c8c60acbff00a0c7d4bb6942
Glyn Hanmer <glynhanmer@gmail.com>
2017-09-23 14:17:51 +0200

    GOH_BASH_PROFILE -> GOHBASHPROFILE

--- bash/install.sh
+++ bash/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #run this script from within the gohbashprofile directory
-#TODO Check for GOH_BASH_PROFILE and handle if already exists. Assume that it was previously installed through script so that the format will be exactly the same, i.e. GOH_BASH_PROFILE='LOCATION'. Just to a replace of LOCATION with $(pwd)
-GOH_BASH_PROFILE="$(pwd)/gohbashprofile"
+#TODO Check for GOHBASHPROFILE and handle if already exists. Assume that it was previously installed through script so that the format will be exactly the same, i.e. GOHBASHPROFILE='LOCATION'. Just to a replace of LOCATION with $(pwd)
+GOHBASHPROFILE="$(pwd)/gohbashprofile"
 profile=".bash_profile"
-printf "\nGOH_BASH_PROFILE='$GOH_BASH_PROFILE'\n. \${GOH_BASH_PROFILE}" >> ~/$profile
-echo "Profile located at $GOH_BASH_PROFILE"
+printf "\nGOHBASHPROFILE='$GOHBASHPROFILE'\n. \${GOHBASHPROFILE}" >> ~/$profile
+echo "Profile located at $GOHBASHPROFILE"
 echo "Sourcing profile..."
-. "$GOH_BASH_PROFILE"
+. "$GOHBASHPROFILE"
 echo "Done"
