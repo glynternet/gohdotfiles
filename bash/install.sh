565b1cce6878e77562924505d1ed61cd36c6a9c4
Glyn Owen Hanmer <glynhanmer@gmail.com>
2017-09-23 15:29:48 +0200

    Merge and additions from origin

--- bash/install.sh
+++ bash/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #run this script from within the gohbashprofile directory
-#TODO Check for GOH_BASH_PROFILE and handle if already exists. Assume that it was previously installed through script so that the format will be exactly the same, i.e. GOH_BASH_PROFILE='LOCATION'. Just to a replace of LOCATION with $(pwd)
-GOH_BASH_PROFILE="$(pwd)/gohbashprofile"
-printf "\nGOH_BASH_PROFILE='$GOH_BASH_PROFILE'\n. \${GOH_BASH_PROFILE}" >> ~/.bashrc
-echo "$GOH_BASH_PROFILE"
-. "$GOH_BASH_PROFILE"
+#TODO Check for GOHBASHPROFILE and handle if already exists. Assume that it was previously installed through script so that the format will be exactly the same, i.e. GOHBASHPROFILE='LOCATION'. Just to a replace of LOCATION with $(pwd)
+GOHBASHPROFILE="$(pwd)/gohbashprofile"
+profile=".bash_profile"
+echo "Adding gohbashprofile to ~/$profile"
+printf "\nGOHBASHPROFILE='$GOHBASHPROFILE'\n. \${GOHBASHPROFILE}" >> ~/$profile
+echo "Profile located at $GOHBASHPROFILE"
+echo "Sourcing profile..."
+. "$GOHBASHPROFILE"
+echo "Done"
