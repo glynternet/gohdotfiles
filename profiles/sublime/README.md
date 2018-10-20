cf6562afb59929ea760429d5ac98a014aa501ddd
Glyn Owen Hanmer <glynhanmer@gmail.com>
2018-10-20 21:16:35 +0100

    Alter sublime profile into Makefile framework

--- profiles/sublime/README.md
+++
@@ -1,2 +0,0 @@
-# Sublime Text
-Run ```install.sh``` from repository directory to move files to ```~/Library/Application Support/Sublime Text 3/Packages/User/```
--- profiles/sublime/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp -v {.,~/'Library/Application Support/Sublime Text 3/Packages/User'}/Preferences.sublime-settings
-cp -v {.,~/'Library/Application Support/Sublime Text 3/Packages/User'}/'Default (OSX).sublime-keymap'
